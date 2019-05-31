82af26e00c692cd7e24d62f7d67ff6f2302675ce
ummsolih <ummsolih@gmail.com>
2019-05-31 23:46:15 +0600

    Домашняя работа

---
+++ HW1_easy.py
@@ -0,0 +1,26 @@
+#Husainova Alina
+#Zadanie 1
+
+# num = 1
+# while num <= 50:
+#     print(num)
+#     num = num + 1
+
+#zadanie 2
+# var1 = int(input('Введите стоимость хлеба '))
+# var2 = int(input('Введите стоимость молока '))
+# var1 = var1 + var2
+# var2 = var1 - var2
+# var1 = var1 - var2
+# print(var1)
+
+#zadanie 3
+age = int(input('Please, enter your age '))
+if age >= 18:
+    print('Access allowed ')
+else:
+    print('Access denied')
+
+
+
+
---
+++ HW1_normal.py
@@ -0,0 +1,24 @@
+# Husainova Alina
+#zadanie 1
+
+# num = int(input('Введите число: '))
+# max1 = 0
+# while num > 0:
+#     x = num % 10
+#     if max1 < x:
+#         max1 = x
+#     num = int(num / 10)
+# print('Самая большая цифра в этом числе: ', max1)
+
+#zadanie 2
+
+a = int(input('Введите значения для переменной a: '))
+b = int(input('Введите значения для переменной b: '))
+a = a + b
+b = a - b
+a = a - b
+print('a равно: ', a, 'b равно: ', b)
+
+
+
+
